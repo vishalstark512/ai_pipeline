0d2b0c5848219d19765e6a33346c7b477a7feee9
vishalstark512 <vishalrajput@iiitdmj.ac.in>
2023-04-26 23:59:09 +0200

    v0

---
+++ app_docker/app/unet_inferrer.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+from utils.plot_image import display
+
+
+class UnetInferrer:
+    def __init__(self):
+        self.image_size = 128
+        self.saved_path = "saved_model/unet.hdf5"
+        self.model = tf.keras.saving.load_model(self.saved_path)
+
+    def preprocess(self, image):
+        image = tf.image.resize(image, (self.image_size, self.image_size))
+        return tf.cast(image, tf.float32) / 255.0
+    
+    def infer(self, image=None):
+        tensor_image = tf.convert_to_tensor(image, dtype=tf.float32)
+        tensor_image = self.preprocess(tensor_image)
+        shape = tensor_image.shape
+        tensor_image = tf.reshape(tensor_image, [1, shape[0], shape[1], shape[2]])
+        pred = self.model.predict(tensor_image)
+        print(pred.shape)
+        display([tensor_image[0], pred[0]])
+        print("XXXXX")
+        pred = pred.tolist()
+        return {"segmentation_output": pred}
